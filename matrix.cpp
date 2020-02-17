6f516719915c6b554be5d327b6bb3222c9da464a
Edilson <eddylson.londonio@gmail.com>
2020-02-17 13:01:38 -0500

    threads

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <pthread.h>
+#include <unistd.h>
 using namespace std;
+
 
 
 
@@ -10,7 +13,7 @@ vector<vector<int>> matrix1 = {
     {3,0,0,4},
     {5,1,1,5},
     {6,4,3,6}
-} ;
+};
 
 vector<vector<int>> matrix2 = {
     {1,0,1,1},
@@ -19,6 +22,7 @@ vector<vector<int>> matrix2 = {
     {2,3,4,4}
 };
 
+vector<vector<int>> result;
 
 
 
@@ -37,44 +41,75 @@ void matrixprint (vector<vector<int>>  &v){
     }
 }
 
-int addElemets(int i){
-    
-}
-
-vector<vector<int>>  add (){
-    int sum = 0;
-    vector<vector<int>> result;
-    result.resize(matrix1.size(),vector<int>(matrix1.size()));
-    for(int i = 0 ; i < matrix2.size(); i++){
-        for (int im1 = 0;im1 < matrix1.size();im1++){
-            for(int pos = 0; pos < matrix1[im1].size();pos++){
-                printf("%d %d \n", matrix1[im1][pos],matrix2[pos][i]);
-                sum  = sum + matrix1[im1][pos] * matrix2[pos][i];
+void *add_elemets(void *i){
+    long mi = long(i);
+    cout<<endl<<"thread number "<< mi <<endl;
+     for (long im1 = 0;im1 < matrix1.size();im1++){
+         int sum = 0;
+            for(long pos = 0; pos < matrix1[im1].size();pos++){
+                sum  = sum + matrix1[im1][pos] * matrix2[pos][mi];
+                cout <<"sum : "<< sum <<endl;
                 
             }
-            printf("esta es la suma %d \n" , sum);
-            result[im1][i] = sum;
-            sum=0;
+            result[im1][mi] = sum;
+            cout<< "result here : " << endl;
+            matrixprint(result);
+            
             
         }
-       
-    }
+        
+        pthread_exit(NULL);
+        
+}
+
+void  add (){
+
     
-    return  result;
+    pthread_t threads[matrix2.size()];
+    int rc;
+    for(long i = 0 ; i < matrix2.size(); i++){
+       rc = pthread_create(&threads[i],NULL,add_elemets, (void *)i );
+       
+       cout <<endl;
+        if(rc){
+            cout<<"error form thread_create";
+            exit(-1);
+        }
+         cout<<"response in thread"<<endl;
+        matrixprint(result);
+    }
+  
+   
+   
 }
 
 void *test (){
+    while(1)
+    {
     for (int i=0; i< matrix1.size() ; i++){
         for (int j = 0; j<matrix1[i].size();j++){
             cout << matrix1[j][i] << endl;
         }
     }
+    }
 }
 
 int main (){
+    cout << "matrix 1"<<endl;
+    matrixprint(matrix1);
+    cout << "matrix 2"<<endl;
+    matrixprint(matrix2);
+    
+    result.resize(matrix1.size(),vector<int>(matrix1.size()));
+    add();
    
-    vector<vector<int>> result = add();
-    matrixprint(result);
+    
+        cout<<endl;
+        //matrixprint(result);
+        cout<<endl;
+    
+        
+      pthread_exit(NULL);
     return 1;
 }
 
