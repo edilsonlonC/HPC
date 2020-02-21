6423765462b153508e07baf7d2096c495e6a1b0b
Edilson <eddylson.londonio@gmail.com>
2020-02-20 22:55:27 -0500

    bash script

---
+++ matrix-mult/threads/execute.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+names='holaaaa'
+# Command for compile c++ threads and run 
+compile='compile' # compile file 
+run='run' # run file 
+#if you use both can compile and run
+args=("$@")
+
+for i in "$@";
+do
+	file="$i"
+done
+
+if [ "${args[0]}" == "$compile" ] &&  [ "${args[1]}" == "$run" ]; 
+then
+	 g++ -std=c++11 -o ${args[2]} ${args[2]}.cpp -lpthread
+	./${args[@]:2:10}
+
+elif [ "$1" == "$run" ];
+then
+	./${args[@]:1:10}
+elif [ "$1" == "$compile" ];
+then
+	g++ -std=c++11 -o ${args[1]} ${args[1]}.cpp -lpthread
+
+else 
+	echo "revisa los comandos de entrada"
+
+fi
