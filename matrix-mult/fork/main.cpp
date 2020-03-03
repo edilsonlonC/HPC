6ef676cb34e18e1392fd56addab39958bcc3cce2
Edilson <eddylson.londonio@gmail.com>
2020-03-03 09:31:45 -0500

    fork folder

---
+++ matrix-mult/fork/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "matrix.h"
+using namespace std;
+
+
+int main (int argc, char *argv[]){
+
+    int n = 0;
+    try{
+        n = stoi(argv[1]);
+    }catch(const exception e){
+        cout<<"me dañe =("<<endl;
+        return -1;
+    }
+
+    MatrixOperation matrix_operation(n);
+    matrix_operation.add();
+   
+}
+
